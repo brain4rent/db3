ca2949fc50b02c2a9942acb05ce89758ed30a0e0
brain4rent <daniel-bernhardt@hotmail.com>
2014-08-10 22:41:45 +0000

    modified user controller

---
+++ splatter/config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Splatter::Application.config.secret_token = '34b272330df560df3aff3fa9bae0998e4424c2aad11d70149b1f4a253320f3d5063649e9fcd5f97c3ae5a655362443dc72013308d3e3bc857767126dd5485c7a'
