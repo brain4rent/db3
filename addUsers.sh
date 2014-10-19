65e09d9bd48562aa7a5b727fe00f51c58b927f5f
brain4rent <daniel-bernhardt@hotmail.com>
2014-10-19 22:54:08 +0000

    Added testing script

--- addUsers.sh
+++ addUsers.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 #Author: 		Dan Bernhardt
-#Date:			24/09/2014
 #Paper:			Databases 3
-#Purpose:		Contains curl commands to add 3 new users
+
 
 #Create 3 users
 curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/users -d '{"user": {"email":"bob@test.com", "name":"Bob", "password":"foo"}}'
