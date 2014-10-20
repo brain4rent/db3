4a6e57963e869696ec8c6544da950e031ba4bd74
brain4rent <daniel-bernhardt@hotmail.com>
2014-10-20 17:50:02 +1300

    Test Script tested and working

--- addSplattsAndFollows.sh
+++ addSplattsAndFollows.sh
@@ -4,26 +4,26 @@
 #Paper:			Databases 3
 
 #Creates 5 splatts for each user in a manner which produces mixes in chronological order
-curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"splatt": {"body":"Hello, Splatter world", "user_id":54444eb069702d31ec000000}}'
-curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"splatt": {"body":"Well hello there", "user_id":54444eb069702d31ec010000}}'
-curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"splatt": {"body":"First Post", "user_id":54444eb069702d31ec020000}}'
-curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"splatt": {"body":"Second Post", "user_id":54444eb069702d31ec020000}}'
-curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"splatt": {"body":"I can post", "user_id":54444eb069702d31ec010000}}'
-curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"splatt": {"body":"So much to do", "user_id":54444eb069702d31ec000000}}'
-curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"splatt": {"body":"So little time", "user_id":54444eb069702d31ec000000"}}'
-curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"splatt": {"body":"Third Post", "user_id":54444eb069702d31ec020000}}'
-curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"splatt": {"body":"Fourth Post", "user_id":54444eb069702d31ec020000}}'
-curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"splatt": {"body":"Another post", "user_id":54444eb069702d31ec010000}}'
-curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"splatt": {"body":"Fifth Post", "user_id":54444eb069702d31ec020000}}'
-curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"splatt": {"body":"And yet another", "user_id":54444eb069702d31ec010000}}'
-curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"splatt": {"body":"Still posting", "user_id":54444eb069702d31ec010000}}'
-curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"splatt": {"body":"IM YELLING", "user_id":54444eb069702d31ec000000}}'
-curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"splatt": {"body":"Now Im not", "user_id":54444eb069702d31ec000000}}'
+curl -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"body":"Hello, Splatter world", "user_id":"54444eb069702d31ec000000"}'
+curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"body":"Well hello there", "user_id":"54444eb069702d31ec010000"}'
+curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"body":"First Post", "user_id":"54444eb069702d31ec020000"}'
+curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"body":"Second Post", "user_id":"54444eb069702d31ec020000"}'
+curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"body":"I can post", "user_id":"54444eb069702d31ec010000"}'
+curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"body":"So much to do", "user_id":"54444eb069702d31ec000000"}'
+curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"body":"So little time", "user_id":"54444eb069702d31ec000000"}'
+curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"body":"Third Post", "user_id":"54444eb069702d31ec020000"}'
+curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"body":"Fourth Post", "user_id":"54444eb069702d31ec020000"}'
+curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"body":"Another post", "user_id":"54444eb069702d31ec010000"}'
+curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"body":"Fifth Post", "user_id":"54444eb069702d31ec020000"}'
+curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"body":"And yet another", "user_id":"54444eb069702d31ec010000"}'
+curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"body":"Still posting", "user_id":"54444eb069702d31ec010000"}'
+curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"body":"IM YELLING", "user_id":"54444eb069702d31ec000000"}'
+curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/splatts -d '{"body":"Now Im not", "user_id":"54444eb069702d31ec000000"}'
 
 #Task 3
 #Causes first user to follow the other 2
-curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/users/follows -d '{"follower_id":54444eb069702d31ec000000, "followed_id":54444eb069702d31ec010000}'
-curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/users/follows -d '{"follower_id":54444eb069702d31ec000000, "followed_id":54444eb069702d31ec020000}'
+curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/users/follows -d '{"id":"54444eb069702d31ec000000", "follows_id":"54444eb069702d31ec010000"}'
+curl -i -H "Content-type: application/json" -X POST http://bernhardt.sqrawler.com:3000/users/follows -d '{"id":54444eb069702d31ec000000, "followed_id":54444eb069702d31ec020000}'
 
 #Task 4
 #Gets the first users splatts
