09db68741737d2cd4ad64210d4eaeab8b289a600
brain4rent <daniel-bernhardt@hotmail.com>
2014-11-12 18:47:08 +1300

    Added routes for riak - completed

--- splatter/config/routes.rb
+++ splatter/config/routes.rb
@@ -1,67 +1,14 @@
 Splatter::Application.routes.draw do
-  put 'usesrs/:id' => 'users#update', :constraints => {:id => /[0-9A-Za-z\-\.@]+/ }
-  resources :splatts, except: [:new, :edit]
-  get 'users/:id' => 'users#show', :constraints => {:id => /[0-9A-Za-z\-\.\@]+/}
-  resources :users, except: [:new, :edit]
-  get 'users/splatts/:id' => 'users#splatts'
-  get 'users/follows/:id' => 'users#show_follows'
-  get 'users/followers/:id' => 'users#show_followers'
+  resources :splatts
+  resources :users
+  
+  put 'users/:id' => 'users#update', :constraints => {:id => /[0-9A-Za-z\-\.\@]+/ }
+  get 'users/splatts/:id' => 'users#splatts', :constraints => {:id => /[0-9A-Za-z\-\.\@]+/}
+  get 'users/follow/:id' => 'users#show_follows', :constraints => {:id => /[0-9A-Za-z\-\.\@]+/ }
+  get 'users/follower/:id' => 'users#show_followers', :constraints => {:id => /[0-9A-Za-z\-\.\@]+/ }
   post 'users/follows' => 'users#add_follows'
-  delete 'users/follows/:id/:follows_id' => 'users#delete_follows'
-  get 'users/splatts-feed/:id' => 'users#splatts_feed'
+  delete 'users/follows/:id/:followed_id' => 'users#delete_follows', :constraints => {:id => /[0-9A-Za-z\-\.\@]+/, :followed_id => /[0-9A-Za-z\-\.\@]+/}
+  get 'users/splatts-feed/:id' => 'users#splatts_feed', :constraints => {:id => /[0-9A-Za-z\-\.\@]+/ }
+  delete 'splatts/:id/:user_id' => 'splatts#destroy', :constraints => {:user_id => /[0-9A-Za-z\-\.\@]+/ }
   
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-  
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
