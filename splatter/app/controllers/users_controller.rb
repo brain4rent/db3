b405f9d5cc76e520bf0280deb735a8639c875251
brain4rent <daniel-bernhardt@hotmail.com>
2014-11-12 19:13:28 +1300

    Added methods for riak

--- splatter/app/controllers/users_controller.rb
+++ splatter/app/controllers/users_controller.rb
@@ -6,53 +6,54 @@ before_filter :set_headers
   # Returns a list of all users
   def index
     db = UserRepository.new(Riak::Client.new)
-    @users = db.bucket("users")
-
-    render json: @users
+    @users = db.all
   end
 
   # GET /users/[:id]
   # Returns user with specified id
   def show
     db = UserRepository.new(Riak::Client.new)
-    @user = db.find(params[:id])
-    render json: @user
+    if !@user.errors
+      @user = db.find(params[:id])
+    else
+      render json: @user.errors, status: :not_found
+    end
   end
 
   # POST /users
   # Creates a user - using POST body
   def create
-    @user = User.new
-    @user.email = params[:email]
-    @user.name = params[:name]
-    @user.password = params[:password]
-    @user.blurb = params[:blurb]
-
     db = UserRepository.new(Riak::Client.new)
+
+    @user = User.new
+    @user.email = params[:user][:email]
+    @user.name = params[:user][:name]
+    @user.password = params[:user][:password]
+    @user.blurb = params[:user][:blurb]
+
     if db.save(@user)
-      render json: @user, status: :created, location: @user
+      render "show", status: :created, location: @user
     else
-      render json: "error", status: :unprocessable_entity
+      render json: @user.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /users/[:id]
   # Updates a user with specified id - using PUT body
   def update
-    @user = User.find(params[:id])
-
-    if @user.update(user_params(params[:user]))
+    db = UserRepository.new(Riak::Client.new)
+    if db.update(params[:id], user_params(params[:user]))
       head :no_content
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: "Failed to update", status: :unprocessable_entity
     end
   end
 
   # DELETE /users/[:id]
   # Deletes user with specified id
   def destroy
-    @user = User.find(params[:id])
-    @user.destroy
+    db = UserRepository.new(Riak::Client.new)
+    db.delete(params[:id])
 
     head :no_content
   end
@@ -65,55 +66,61 @@ before_filter :set_headers
   # GET /users/splatts/[:id]
   # Returns splatts for user with specified id
   def splatts
-	@user = User.find(params[:id])
-	
-	render json: @user.splatts
+    db = UserRepository.new(Riak::Client.new)
+    @user = db.find(params[:id])
+    db = SplattRepository.new(Riak::Client.new, @user)
+    @splatts = db.all
   end
   
   # GET /users/follows/[:id]
   # Returns list of users followed by specified user
   def show_follows
-	@user = User.find(params[:id])
-	
-	render json: @user.follows
+    db = UserRepository.new(Riak::Client.new)
+    @user = db.find(params[:id])
   end
   
   # GET /users/followers/[:id]
   # Returns list of user who follow user with specified id
   def show_followers
-	@user = Users.find(params[:id])
-	
-	render json: @user.followed_by
+    db = UserRepository.new(Riak::Client.new)
+    @user = db.find(params[:id])
   end
   
   # POST /users/follows
   # Creates a follower/followed relationship - using POST body
   def add_follows
-	@user = User.find(params[:follower_id])
-	@user_to_follow = User.find(params[:followed_id])
-
-	@user.follows << @user_to_follow
-	
-	render json: @user.follows
+    db = UserRepository.new(Riak::Client.new)
+    @user = db.find(params[:id])
+    @user_to_follow = db.find(params[:follows_id])
+    
+    if db.follow(@user, @user_to_follow)
+      render "show_follows"
+    else
+      render json: "error saving follow relationship", status: :unprocessable_entity
+    end
   end
   
   # DELETE /users/follows/[:id1]/[:id2]
   # Causes user with id1 to unfollow the user with id2
   def delete_follows
-	@user = User.find(params[:id])
-	@user_to_delete = User.find(params[:follows_id])
-	
-	@user.follows.delete(@user_to_delete)
-	
-	render json: @user.follows
+    db = UserRepository.new(Riak::Client.new)
+    @user = db.find(params[:id])
+    @user_to_unfollow = db.find(params[:followed_id])
+    
+    if db.unfollow(@user, @user_to_unfollow)
+      render "show_follows"
+    else
+      render json: "error removing follow relationship", status: :unprocessable_entity
+    end
   end
   
   # GET /users/splatts-feed/[:id]
   #Returns the splatts feed for the user with the specified id
   def splatts_feed
-  	@feed = Splatt.find_by_sql(["SELECT * FROM splatts JOIN follows ON splatts.user_id = follows.followed_id JOIN users ON follows.follower_id = users.id WHERE users.id = ?", params[:id]])
+    @id = params[:id]
+    db = UserRepository.new(Riak::Client.new)
+    @feed = db.splatts_feed(@id) 
 
-  
   end
 
   def set_headers
